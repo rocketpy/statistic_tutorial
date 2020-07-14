781e16dbd4526d93ed2a01687f2754bce590b12f
rocketpy <dimitr.ushakov@gmail.com>
2020-07-14 21:43:29 +0300

    updated

--- statistic_visualize.py
+++ statistic_visualize.py
@@ -1,5 +1,29 @@
 #  Altair - declarative statistical visualization library for Python 
-#  Examples of Charts : https://altair-viz.github.io/gallery/index.html
+#  Examples of Charts , Histograms , Maps , Interactive Charts , Case Studies : https://altair-viz.github.io/gallery/index.html
 
 
+# code example , area chart with Gradient , taked from : https://altair-viz.github.io/getting_started/overview.html
 
+import altair as alt
+from vega_datasets import data
+
+
+source = data.stocks()
+
+alt.Chart(source).transform_filter(
+    'datum.symbol==="GOOG"'
+).mark_area(
+    line={'color':'darkgreen'},
+    color=alt.Gradient(
+        gradient='linear',
+        stops=[alt.GradientStop(color='white', offset=0),
+               alt.GradientStop(color='darkgreen', offset=1)],
+        x1=1,
+        x2=1,
+        y1=1,
+        y2=0
+    )
+).encode(
+    alt.X('date:T'),
+    alt.Y('price:Q')
+)
